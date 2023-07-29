733f0bd3b4e95dcd6c0712606611e4b9843405e1
pipeleon <felipe.leon@datasketch.co>
2023-07-29 14:43:28 -0600

    adding task 7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Task 4"""
+
+
+def recursive_look(matrix, list_shape):
+    """Auxiliar recursive funtion"""
+    if type(matrix) == list:
+        list_shape.append(len(matrix))
+        if len(matrix) > 0:
+            return recursive_look(matrix[0], list_shape)
+        else:
+            return list_shape
+    else:
+        return list_shape
+
+
+def matrix_shape(matrix):
+    """Funtion to get the shape of a matrix"""
+    shape = []
+
+    return recursive_look(matrix, shape)
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """Funtion to sum 2D arrays"""
+    new_matrix = []
+    for row in mat1:
+        new_matrix.append(row.copy())
+
+    if axis == 0:
+        if matrix_shape(mat1)[1] != matrix_shape(mat2)[1]:
+            return None
+        
+        for i in range(len(mat2)):
+            new_matrix.append(mat2[i])
+    elif axis == 1:
+        if matrix_shape(mat1)[0] != matrix_shape(mat2)[0]:
+            return None
+
+        for i in range(matrix_shape(mat1)[0]):
+            new_matrix[i].append(mat2[i][0])
+
+    return new_matrix
