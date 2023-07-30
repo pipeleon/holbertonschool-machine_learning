70bbc722fddccda7bfb32de3883e1faa958e2f78
pipeleon <felipe.leon@datasketch.co>
2023-07-29 19:00:01 -0600

    adding task 101

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -27,7 +27,7 @@ def add_matrices(mat1, mat2):
 
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
-    
+
     if len(matrix_shape(mat1)) == 1:
         for i in range(matrix_shape(mat1)[0]):
             new_matrix.append(mat1[i] + mat2[i])
@@ -36,6 +36,27 @@ def add_matrices(mat1, mat2):
             temp = []
             for j in range(matrix_shape(mat1)[1]):
                 temp.append(mat1[i][j] + mat2[i][j])
+            new_matrix.append(temp)
+    elif len(matrix_shape(mat1)) == 3:
+        for i in range(matrix_shape(mat1)[0]):
+            temp = []
+            for j in range(matrix_shape(mat1)[1]):
+                temp2 = []
+                for k in range(matrix_shape(mat1)[2]):
+                    temp2.append(mat1[i][j][k] + mat2[i][j][k])
+                temp.append(temp2)
+            new_matrix.append(temp)
+    elif len(matrix_shape(mat1)) == 4:
+        for i in range(matrix_shape(mat1)[0]):
+            temp = []
+            for j in range(matrix_shape(mat1)[1]):
+                temp2 = []
+                for k in range(matrix_shape(mat1)[2]):
+                    temp3 = []
+                    for l in range(matrix_shape(mat1)[3]):
+                        temp3.append(mat1[i][j][k][l] + mat2[i][j][k][l])
+                    temp2.append(temp3)
+                temp.append(temp2)
             new_matrix.append(temp)
 
     return new_matrix
