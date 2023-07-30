4c1c55432c47538decd69bca339531cdc438a4bf
pipeleon <felipe.leon@datasketch.co>
2023-07-29 18:54:06 -0600

    adding task 101

---
+++ math/linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Task 100"""
+
+
+def recursive_look(matrix, list_shape):
+    """Auxiliar recursive funtion"""
+    if type(matrix) == list:
+        list_shape.append(len(matrix))
+        if len(matrix) > 0:
+            return recursive_look(matrix[0], list_shape)
+        else:
+            return list_shape
+    else:
+        return list_shape
+
+
+def matrix_shape(matrix):
+    """Funtion to get the shape of a matrix"""
+    shape = []
+
+    return recursive_look(matrix, shape)
+
+
+def add_matrices(mat1, mat2):
+    """Funtion to sum 2D arrays"""
+    new_matrix = []
+
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    
+    if len(matrix_shape(mat1)) == 1:
+        for i in range(matrix_shape(mat1)[0]):
+            new_matrix.append(mat1[i] + mat2[i])
+    elif len(matrix_shape(mat1)) == 2:
+        for i in range(matrix_shape(mat1)[0]):
+            temp = []
+            for j in range(matrix_shape(mat1)[1]):
+                temp.append(mat1[i][j] + mat2[i][j])
+            new_matrix.append(temp)
+
+    return new_matrix
