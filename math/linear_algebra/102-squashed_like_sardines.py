f5e9957ad22c2766f06fbc4fc3619138f130e18b
pipeleon <felipe.leon@datasketch.co>
2023-07-30 21:12:32 -0600

    adding task 102

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -23,13 +23,13 @@ def matrix_shape(matrix):
 
 def cat_matrices(mat1, mat2, axis=0):
     """Funtion to concat arrays"""
-    
+
     if len(matrix_shape(mat1)) == 1 and len(matrix_shape(mat2)) == 1:
         new_matrix = mat1.copy()
 
         for i in range(matrix_shape(mat2)[0]):
             new_matrix.append(mat2[i])
-    else:
+    elif len(matrix_shape(mat1)) == 2 and len(matrix_shape(mat2)) == 2:
         new_matrix = []
         for row in mat1:
             new_matrix.append(row.copy())
@@ -47,5 +47,77 @@ def cat_matrices(mat1, mat2, axis=0):
             for i in range(matrix_shape(mat2)[0]):
                 for j in range(matrix_shape(mat2)[1]):
                     new_matrix[i].append(mat2[i][j])
+    elif len(matrix_shape(mat1)) == 3 and len(matrix_shape(mat2)) == 3:
+        new_matrix = []
+        for ax0 in mat1:
+            temp = []
+            for ax1 in ax0:
+                temp.append(ax1.copy())
+            new_matrix.append(temp)
 
+        if axis == 0:
+            if matrix_shape(mat1)[1] != matrix_shape(mat2)[1] and matrix_shape(mat1)[2] != matrix_shape(mat2)[2]:
+                return None
+
+            for i in range(len(mat2)):
+                new_matrix.append(mat2[i])
+        elif axis == 1:
+            if matrix_shape(mat1)[0] != matrix_shape(mat2)[0] and matrix_shape(mat1)[2] != matrix_shape(mat2)[2]:
+                return None
+
+            for i in range(matrix_shape(mat2)[0]):
+                for j in range(matrix_shape(mat2)[1]):
+                    new_matrix[i].append(mat2[i][j])
+        elif axis == 2:
+            if matrix_shape(mat1)[0] != matrix_shape(mat2)[0] and matrix_shape(mat1)[1] != matrix_shape(mat2)[1]:
+                return None
+
+            for i in range(matrix_shape(mat2)[0]):
+                for j in range(matrix_shape(mat2)[1]):
+                    for k in range(matrix_shape(mat2)[2]):
+                        new_matrix[i][j].append(mat2[i][j][k])
+    elif len(matrix_shape(mat1)) == 4 and len(matrix_shape(mat2)) == 4:
+        new_matrix = []
+        for ax0 in mat1:
+            temp = []
+            for ax1 in ax0:
+                temp2 = []
+                for ax2 in ax1:
+                    temp2.append(ax2.copy())
+                temp.append(temp2)
+            new_matrix.append(temp)
+
+        if axis == 0:
+            if matrix_shape(mat1)[1] != matrix_shape(mat2)[1] and matrix_shape(mat1)[2] != matrix_shape(mat2)[2] and matrix_shape(mat1)[3] != matrix_shape(mat2)[3]:
+                return None
+
+            for i in range(len(mat2)):
+                new_matrix.append(mat2[i])
+        elif axis == 1:
+            if matrix_shape(mat1)[0] != matrix_shape(mat2)[0] and matrix_shape(mat1)[2] != matrix_shape(mat2)[2] and matrix_shape(mat1)[3] != matrix_shape(mat2)[3]:
+                return None
+
+            for i in range(matrix_shape(mat2)[0]):
+                for j in range(matrix_shape(mat2)[1]):
+                    new_matrix[i].append(mat2[i][j])
+        elif axis == 2:
+            if matrix_shape(mat1)[0] != matrix_shape(mat2)[0] and matrix_shape(mat1)[1] != matrix_shape(mat2)[1] and matrix_shape(mat1)[3] != matrix_shape(mat2)[3]:
+                return None
+
+            for i in range(matrix_shape(mat2)[0]):
+                for j in range(matrix_shape(mat2)[1]):
+                    for k in range(matrix_shape(mat2)[2]):
+                        new_matrix[i][j].append(mat2[i][j][k])
+        elif axis == 3:
+            if matrix_shape(mat1)[0] != matrix_shape(mat2)[0] and matrix_shape(mat1)[1] != matrix_shape(mat2)[1] and matrix_shape(mat1)[2] != matrix_shape(mat2)[2]:
+                return None
+
+            for i in range(matrix_shape(mat2)[0]):
+                for j in range(matrix_shape(mat2)[1]):
+                    for k in range(matrix_shape(mat2)[2]):
+                        for i2 in range(matrix_shape(mat2)[3]):
+                            new_matrix[i][j][k].append(mat2[i][j][k][i2])
+    else:
+        return None
+    
     return new_matrix
