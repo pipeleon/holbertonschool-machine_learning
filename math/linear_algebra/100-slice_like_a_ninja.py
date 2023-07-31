42b97da0c06efbcfc28eee3a145092de688c4869
pipeleon <felipe.leon@datasketch.co>
2023-07-31 15:03:59 -0600

    adding task 100

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -8,8 +8,30 @@ def np_slice(matrix, axes={}):
 
     new_matrix = []
 
+    if len(shape) >= 5:
+        new_matrix = matrix[:, :, :, :, :]
+        slice4 = axes.get(4)
+        if slice4:
+            if len(slice4) == 1:
+                new_matrix = new_matrix[:, :, :, :, :slice4[0]]
+            elif len(slice4) == 2:
+                new_matrix = new_matrix[:, :, :, :, slice4[0]:slice4[1]]
+            elif len(slice4) == 3:
+                new_matrix = new_matrix[:, :, :, :, slice4[0]:slice4[1]:slice4[2]]
+    if len(shape) >= 4:
+        if len(new_matrix) == 0:
+            new_matrix = matrix[:, :, :, :]
+        slice3 = axes.get(3)
+        if slice3:
+            if len(slice3) == 1:
+                new_matrix = new_matrix[:, :, :, :slice3[0]]
+            elif len(slice3) == 2:
+                new_matrix = new_matrix[:, :, :, slice3[0]:slice3[1]]
+            elif len(slice3) == 3:
+                new_matrix = new_matrix[:, :, :, slice3[0]:slice3[1]:slice3[2]]
     if len(shape) >= 3:
-        new_matrix = matrix[:,:,:]
+        if len(new_matrix) == 0:
+            new_matrix = matrix[:, :, :]
         slice2 = axes.get(2)
         if slice2:
             if len(slice2) == 1:
@@ -19,7 +41,8 @@ def np_slice(matrix, axes={}):
             elif len(slice2) == 3:
                 new_matrix = new_matrix[:, :, slice2[0]:slice2[1]:slice2[2]]
     if len(shape) >= 2:
-        if len(new_matrix) == 0: new_matrix = matrix[:,:]
+        if len(new_matrix) == 0:
+            new_matrix = matrix[:, :]
         slice1 = axes.get(1)
         if slice1:
             if len(slice1) == 1:
@@ -29,7 +52,8 @@ def np_slice(matrix, axes={}):
             elif len(slice1) == 3:
                 new_matrix = new_matrix[:, slice1[0]:slice1[1]:slice1[2]]
     if len(shape) >= 1:
-        if len(new_matrix) == 0: new_matrix = matrix[:]
+        if len(new_matrix) == 0:
+            new_matrix = matrix[:]
         slice0 = axes.get(0)
         if slice0:
             if len(slice0) == 1:
@@ -38,6 +62,5 @@ def np_slice(matrix, axes={}):
                 new_matrix = new_matrix[slice0[0]:slice0[1]]
             elif len(slice0) == 3:
                 new_matrix = new_matrix[slice0[0]:slice0[1]:slice0[2]]
-    
+
     return new_matrix
-    
