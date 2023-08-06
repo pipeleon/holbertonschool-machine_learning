044795831de14749ae2b25ddb6051ce51a5d9c2f
pipeleon <felipe.leon@datasketch.co>
2023-08-05 18:05:33 -0600

    adding calculus task 17

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 17"""
+
+
+def poly_integral(poly, C=0):
+    """Function that calculates the integral of a polynomial"""
+
+    if type(poly) != list or len(poly) == 0 or type(C) != int:
+        return None
+
+    int_poly = [C]
+
+    for i in range(len(poly)):
+        int_poly.append(poly[i] / (i + 1))
+
+    return int_poly
