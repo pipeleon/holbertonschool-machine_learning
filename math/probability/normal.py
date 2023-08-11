f288fe45b6a379d3729bb88d2372ec61a92e16fd
pipeleon <felipe.leon@datasketch.co>
2023-08-10 19:01:31 -0600

    adding task 7 project probability

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -54,7 +54,9 @@ class Normal():
         self.__stddev = stddev
 
     def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
+        """"Calculates the x-value of a given z-score"""
         return z * self.stddev + self.mean
