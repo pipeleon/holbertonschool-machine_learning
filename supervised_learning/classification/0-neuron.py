29343e99120805fea3191f9481cc64e6157c86f1
pipeleon <oflg94@gmail.com>
2023-08-20 13:37:40 -0500

    adding task 0 project classification

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 0 Classification"""
+import numpy as np
+
+
+class Neuron():
+    """Single neuron performing binary classification"""
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.randn(1, nx) * 0.01
+        self.b = 0
+        self.A = 0
