2bd9fc8a264506df44d68cb87032894cf7f5ecba
pipeleon <oflg94@gmail.com>
2023-08-20 13:40:15 -0500

    adding task 0 project classification

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -11,6 +11,6 @@ class Neuron():
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.W = np.random.randn(1, nx) * 0.01
+        self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
