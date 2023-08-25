68919203912d187868ebcf5bfb1d66be7318fa91
pipeleon <oflg94@gmail.com>
2023-08-24 19:35:49 -0500

    addign task 25 project classification

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Task 25 Classification"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """Converts a one-hot matrix into a vector of labels"""
+    if type(one_hot) is not np.ndarray:
+        return None
+    classes = one_hot.shape[0]
+    lenY = one_hot.shape[1]
+    matrix = np.zeros((lenY, ))
+
+    for i in range(lenY):
+        for j in range(classes):
+            if one_hot[j][i] == 1:
+                matrix[i] = j
+
+    return matrix.astype(int)
