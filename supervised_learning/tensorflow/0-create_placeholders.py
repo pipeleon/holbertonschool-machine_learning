708642f5f72147b296159ecc0e2e90c2f1493910
Felipe León Giraldo <91047947+pipeleon@users.noreply.github.com>
2023-08-27 15:01:36 -0500

    Actualizar 0-create_placeholders.py

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -5,7 +5,7 @@ import tensorflow.compat.v1 as tf
 
 def create_placeholders(nx, classes):
     """Returns: placeholders x and y"""
-    x = tf.placerfolder(tf.float32, shape=(None, nx), name="x")
-    y = tf.placerfolder(tf.float32, shape=(None, classes), name="y")
+    x = tf.placerholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placerholder(tf.float32, shape=(None, classes), name="y")
 
     return x, y
