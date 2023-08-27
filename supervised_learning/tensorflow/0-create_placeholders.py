74b6db8dec0bd44343391b4dad2ba7f8af21875a
pipeleon <oflg94@gmail.com>
2023-08-27 12:43:54 -0500

    adding task 0 tensorflow

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Task 0 Tensorflow"""
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def create_placeholders(nx, classes):
+    """Returns: placeholders x and y"""
+    x = tf.placerfolder(tf.float32, [None, nx])
+    y = tf.placerfolder(tf.float32, [None, classes])
+
+    return x, y
