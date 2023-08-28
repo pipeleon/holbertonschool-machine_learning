a7c08e9640c64dae854e3d56e4e88700b4a53583
pipeleon <oflg94@gmail.com>
2023-08-27 20:34:27 -0500

    adding task 1 project tensorflow

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,9 +7,9 @@ def create_layer(prev, n, activation):
     """Returns: layer for x, n and activation funtion"""
     weights_init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.layers.Dense(
-        units=n, 
-        activation= activation, 
-        name="layer", 
+        units=n,
+        activation=activation,
+        name="layer",
         kernel_initializer=weights_init
     )
 
