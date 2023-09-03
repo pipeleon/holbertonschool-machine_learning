91fac3a649b2f16d5a1eee4754e26ddc0bacb17b
pipeleon <oflg94@gmail.com>
2023-09-03 12:02:57 -0500

    adding task 10 project optimiztion

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 10 Optimization"""
+import tensorflow.compat.v1 as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """
+    Creates the training operation for a neural
+    network in tensorflow using the Adam
+    optimization algorithm
+    """
+
+    optimizer = tf.train.RMSPropOptimizer(alpha, beta1, beta2, epsilon)
+    train = optimizer.minimize(loss)
+
+    return train
