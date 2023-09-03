5f28c7170cd56497bc8721c6bf5b01506883386e
pipeleon <oflg94@gmail.com>
2023-09-03 11:24:21 -0500

    adding task 8 prokect optimiztion

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 8 Optimization"""
+import tensorflow.compat.v1 as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    Creates the training operation for a neural
+    network in tensorflow using the RMSProp
+    optimization algorithm
+    """
+
+    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
+    train = optimizer.minimize(loss)
+
+    return train
