b6e52458c1898449f16ad998c5aebf881366bd76
pipeleon <oflg94@gmail.com>
2023-09-13 21:26:39 -0500

    adding task 3 project keras

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 """Task 3 Keras"""
-import numpy as np
+import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
     """Converts a label vector into a one-hot matrix"""
-    classes = np.max(labels) + 1
-    matrix = np.zeros((classes, len(labels)))
-
-    for i in range(len(labels)):
-        if labels[i] > classes:
-            return None
-        matrix[i][labels[i]] = 1
+    classes = labels.shape[0]
+    matrix = K.utils.to_categorical(labels, classes)
 
     return matrix
