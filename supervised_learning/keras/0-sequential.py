092b0a46fb75dac348d8c4e45e6cd220371642c5
pipeleon <oflg94@gmail.com>
2023-09-13 20:27:00 -0500

    adding task 0 project keras

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -6,4 +6,19 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Builds a neural network with the Keras library"""
     model = K.Sequential()
-    
+    regularizer = K.regularizers.L2(lambtha)
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(K.layers.Dense(layers[i], activations[i],
+                                     input_shape=(nx, ),
+                                     kernel_regularizer=regularizer))
+        else:
+            model.add(K.layers.Dense(layers[i], activation=activations[i],
+                                     kernel_regularizer=regularizer))
+        if i != len(layers) - 1:
+            model.add(K.layers.Dropout(keep_prob))
+
+    model.compile(loss='binary_crossentropy', optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
