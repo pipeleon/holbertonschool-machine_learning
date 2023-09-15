dc1cd337321da1b5eb3c53786eee775fc78bfaeb
pipeleon <oflg94@gmail.com>
2023-09-14 19:50:49 -0500

    adding task 7 project keras

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -8,12 +8,18 @@ def train_model(network, data, labels, batch_size, epochs,
                 patience=0, learning_rate_decay=False,
                 alpha=0.1, decay_rate=1, verbose=True, shuffle=False):
     """Trains a model using mini-batch gradient descent:"""
+    callbacks = []
+
     def scheduler(epoch, lr):
         return alpha / (1 + decay_rate * epoch)
+
+    if early_stopping:
+        callbacks.append(K.callbacks.EarlyStopping(patience=patience))
+
+    if learning_rate_decay:
+        callbacks.append(K.callbacks.LearningRateScheduler(scheduler, 1))
 
     return network.fit(data, labels, batch_size,
                        epochs, verbose, shuffle=shuffle,
                        validation_data=validation_data,
-                       callbacks=[K.callbacks.EarlyStopping(patience=patience),
-                                  K.callbacks.LearningRateScheduler(scheduler,
-                                                                    1)])
+                       callbacks=callbacks)
