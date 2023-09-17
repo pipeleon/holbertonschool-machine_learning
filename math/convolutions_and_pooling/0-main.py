e5e6632bc4568b06166df7a7c3154721bc704424
Felipe León Giraldo <91047947+pipeleon@users.noreply.github.com>
2023-09-17 13:40:22 -0500

    Actualizar 0-main.py

--- math/convolutions_and_pooling/0-main.py
+++ math/convolutions_and_pooling/0-main.py
@@ -1,19 +1,1 @@
-#!/usr/bin/env python3
 
-#!/usr/bin/env python3
-
-import numpy as np
-convolve_grayscale_valid = __import__('0-convolve_grayscale_valid').convolve_grayscale_valid
-
-np.random.seed(0)
-m = np.random.randint(1000, 2000)
-h, w = np.random.randint(100, 200, 2).tolist()
-fh, fw = np.random.randint(3, 10, 2).tolist()
-
-images = np.random.randint(0, 256, (m, h, w))
-kernel = np.random.randint(0, 10, (fh, fw))
-print(images.shape)
-print(kernel.shape)
-conv_ims = convolve_grayscale_valid(images, kernel)
-print(conv_ims)
-print(conv_ims.shape)
