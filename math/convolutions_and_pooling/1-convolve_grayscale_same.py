dd729835e20f6d60d9c949b369725b25ff227a22
pipeleon <oflg94@gmail.com>
2023-09-17 18:46:27 -0500

    adding task 1 for convolutions and pooling project

---
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Task 1 Convolutions and Pooling"""
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """Performs a same convolution on grayscale images"""
+    m = images.shape[0]
+    k1 = kernel.shape[0]
+    k2 = kernel.shape[1]
+    p = (k1 - 1) / 2
+    new_dimX = images.shape[1] - k1 + 1 + 2 * int(p)
+    new_dimY = images.shape[2] - k2 + 1 + 2 * int(p)
+
+    images = np.pad(images, 1, mode="constant")
+    images = images[1:-1, :, :]
+
+    new_image = np.zeros((m, new_dimX, new_dimY))
+
+    for j in range(new_image.shape[1]):
+        for k in range(new_image.shape[2]):
+            new_value = np.sum(images[:, j:j+k1, k:k+k2] * kernel, axis=(1, 2))
+            new_image[:, j, k] = new_value
+
+    return new_image
