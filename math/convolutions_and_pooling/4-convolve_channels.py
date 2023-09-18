1a9b9066b442a04d3587714573de2cb18c4aae5f
pipeleon <oflg94@gmail.com>
2023-09-17 21:38:05 -0500

    adding task 4 for convolutions and pooling project

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -8,10 +8,12 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     m = images.shape[0]
     k1 = kernel.shape[0]
     k2 = kernel.shape[1]
+    s1 = stride[0]
+    s2 = stride[1]
     if type(padding) is str:
         if padding == 'same':
-            p1 = int((k1 - 1) / 2)
-            p2 = int((k2 - 1) / 2)
+            p1 = int((images.shape[1] * (s1 - 1) + k1 - s1) / 2)
+            p2 = int((images.shape[2] * (s2 - 1) + k2 - s2) / 2)
             if k1 % 2 == 0:
                 p1 += 1
             if k2 % 2 == 0:
@@ -22,19 +24,20 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     else:
         p1 = padding[0]
         p2 = padding[1]
-    s1 = stride[0]
-    s2 = stride[1]
+
     new_dimX = int(1 + (images.shape[1] - k1 + 2 * p1) / s1)
     new_dimY = int(1 + (images.shape[2] - k2 + 2 * p2) / s2)
 
-    images = np.pad(images, ((0, 0), (p1, p1), (p2, p2)), mode="constant")
+    images = np.pad(
+        images, ((0, 0), (p1, p1), (p2, p2), (0, 0)), mode="constant")
 
     new_image = np.zeros((m, new_dimX, new_dimY))
 
     for j in range(new_image.shape[1]):
         for k in range(new_image.shape[2]):
             new_value = np.sum(
-                images[:, j*s1:j*s1+k1, k*s2:k*s2+k2] * kernel, axis=(1, 2))
+                images[:, j*s1:j*s1+k1, k*s2:k*s2+k2, :] *
+                kernel, axis=(1, 2, 3))
             new_image[:, j, k] = new_value
 
     return new_image
