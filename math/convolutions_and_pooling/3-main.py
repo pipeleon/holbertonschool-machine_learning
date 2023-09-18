b73825fda511569a5331652cefafd40cdad0345e
pipeleon <oflg94@gmail.com>
2023-09-17 21:16:31 -0500

    adding task 3 for convolutions and pooling project

--- math/convolutions_and_pooling/3-main.py
+++ math/convolutions_and_pooling/3-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import matplotlib.pyplot as plt
+""" import matplotlib.pyplot as plt
 import numpy as np
 convolve_grayscale = __import__('3-convolve_grayscale').convolve_grayscale
 
@@ -17,4 +17,24 @@ if __name__ == '__main__':
     plt.imshow(images[0], cmap='gray')
     plt.savefig("3-normal.png")
     plt.imshow(images_conv[0], cmap='gray')
-    plt.savefig("3-alter.png")
+    plt.savefig("3-alter.png") """
+import numpy as np
+convolve_grayscale = __import__('3-convolve_grayscale').convolve_grayscale
+
+np.random.seed(3)
+m = np.random.randint(100, 200)
+h, w = np.random.randint(20, 50, 2).tolist()
+fh, fw = (np.random.randint(2, 5, 2)).tolist()
+sh, sw = (np.random.randint(2, 4, 2)).tolist()
+
+images = np.random.randint(0, 256, (m, h, w))
+kernel = np.random.randint(0, 10, (fh, fw))
+
+print(images.shape)
+print(kernel.shape)
+print(sh)
+print(sw)
+conv_ims = convolve_grayscale(images, kernel, stride=(sh, sw))
+np.set_printoptions(threshold=np.inf)
+print(conv_ims)
+print(conv_ims.shape)
--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
-"""Task 3 Convolutions and Pooling"""
+"""Task 4 Convolutions and Pooling"""
 import numpy as np
 
 
-def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
-    """Performs a convolution on grayscale images with custom padding"""
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """Performs a convolution on images with channels"""
     m = images.shape[0]
     k1 = kernel.shape[0]
     k2 = kernel.shape[1]
-    s1 = stride[0]
-    s2 = stride[1]
     if type(padding) is str:
         if padding == 'same':
-            p1 = int((images.shape[1] * (s1 - 1) + k1 - s1) / 2)
-            p2 = int((images.shape[2] * (s2 - 1) + k2 - s2) / 2)
+            p1 = int((k1 - 1) / 2)
+            p2 = int((k2 - 1) / 2)
             if k1 % 2 == 0:
                 p1 += 1
             if k2 % 2 == 0:
@@ -24,7 +22,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     else:
         p1 = padding[0]
         p2 = padding[1]
-
+    s1 = stride[0]
+    s2 = stride[1]
     new_dimX = int(1 + (images.shape[1] - k1 + 2 * p1) / s1)
     new_dimY = int(1 + (images.shape[2] - k2 + 2 * p2) / s2)
 
