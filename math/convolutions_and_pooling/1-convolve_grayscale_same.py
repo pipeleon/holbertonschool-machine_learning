204d7795cb7c015b4357793e2231f58acc54b711
pipeleon <oflg94@gmail.com>
2023-09-17 19:44:37 -0500

    adding task 1 for convolutions and pooling project

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -8,12 +8,16 @@ def convolve_grayscale_same(images, kernel):
     m = images.shape[0]
     k1 = kernel.shape[0]
     k2 = kernel.shape[1]
-    p = (k1 - 1) / 2
-    new_dimX = images.shape[1] - k1 + 1 + 2 * int(p)
-    new_dimY = images.shape[2] - k2 + 1 + 2 * int(p)
+    p1 = int((k1 - 1) / 2)
+    p2 = int((k2 - 1) / 2)
+    if k1 % 2 == 0:
+        p1 += 1
+    if k2 % 2 == 0:
+        p2 += 1
+    new_dimX = images.shape[1]
+    new_dimY = images.shape[2]
 
-    images = np.pad(images, 1, mode="constant")
-    images = images[1:-1, :, :]
+    images = np.pad(images, ((0, 0), (p1, p1), (p2, p2)), mode="constant")
 
     new_image = np.zeros((m, new_dimX, new_dimY))
 
--- math/convolutions_and_pooling/1-main.py
+++ math/convolutions_and_pooling/1-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import matplotlib.pyplot as plt
+""" import matplotlib.pyplot as plt
 import numpy as np
 convolve_grayscale_same = __import__('1-convolve_grayscale_same').convolve_grayscale_same
 
@@ -17,4 +17,18 @@ if __name__ == '__main__':
     plt.imshow(images[0], cmap='gray')
     plt.savefig("1-normal.png")
     plt.imshow(images_conv[0], cmap='gray')
-    plt.savefig("2-alter.png")
+    plt.savefig("2-alter.png") """
+
+import numpy as np
+convolve_grayscale_same = __import__('1-convolve_grayscale_same').convolve_grayscale_same
+
+np.random.seed(1)
+m = np.random.randint(1000, 2000)
+h, w = np.random.randint(100, 200, 2).tolist()
+fh, fw = (np.random.randint(1, 5, 2) * 2).tolist()
+
+images = np.random.randint(0, 256, (m, h, w))
+kernel = np.random.randint(0, 10, (fh, fw))
+conv_ims = convolve_grayscale_same(images, kernel)
+print(conv_ims)
+print(conv_ims.shape)
