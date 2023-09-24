2fc59eebbf33bd104c2f25565e0b9efb1698889e
pipeleon <oflg94@gmail.com>
2023-09-23 19:21:22 -0500

    adding task 3 project cn

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -18,7 +18,7 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
             for k in range(dA.shape[2]):
                 for c in range(dA.shape[3]):
                     if mode == "max":
-                        a_slice = A_prev[set, k*s2:k*s2+k2, j*s1:j*s1+k1, c]
+                        a_slice = A_prev[set, j*s1:j*s1+k1, k*s2:k*s2+k2, c]
                         mask = (a_slice == np.max(a_slice))
                         dA_prev[set, j*s1:j*s1+k1,
                                 k*s2:k*s2+k2, c] += (mask * dA[set, j, k, c])
