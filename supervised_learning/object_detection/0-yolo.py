f293856ef3379b6757203bcec9fb0ee749e35064
pipeleon <oflg94@gmail.com>
2023-10-12 10:43:03 -0500

    object detection readme

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Task 0 Keras"""
+import numpy as np
+import tensorflow as tf
+
+
+class Yolo():
+    """NST"""
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+        self.attr = 0
