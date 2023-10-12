7e35d609f5f16db19d396f783cdbb96e307d7ca8
pipeleon <oflg94@gmail.com>
2023-10-12 10:27:42 -0500

    neural_style_transfer readme

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Task 0 Keras"""
-import tensorflow.keras as K
+import numpy as np
 
 
-def build_model(nx, layers, activations, lambtha, keep_prob):
-    
+class NST():
+    """NST"""
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+        self.attr = 0
